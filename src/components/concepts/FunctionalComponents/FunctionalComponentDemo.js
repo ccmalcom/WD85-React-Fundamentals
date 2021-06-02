16c10f5b40527050fd9a7abfde1c4de9ab512be6
ccmalcom <ChaseCMalcom@gmail.com>
2021-06-02 17:16:45 -0400

    began work on module 2

---
+++ src/components/concepts/FunctionalComponents/FunctionalComponentDemo.js
@@ -0,0 +1,10 @@
+/* functional components have the following abilities/traits:
+    -rendering of information with or without changing state
+    -no 'this' keyword. Class components use this extensively.
+    -often used as child components that will recieve props(properties) from a parent component
+    *Functional components are just plain, standard JavaScript functions that return an React element
+*/
+
+const FunctionalComponentDemo = () =>{
+
+}
