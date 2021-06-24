1455c866a58b092da3adfccbbcb8a12e9ce53d3b
Chase Malcom <ChaseCMalcom@gmail.com>
2021-06-24 10:47:28 -0400

    moved api key to .env

--- src/components/apps/nyt-app/NytApp.js
+++ src/components/apps/nyt-app/NytApp.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import NytResults from './NytResults';
 
 const baseURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
-const key = 'DgIVGe24WvyESgpCgb9gS3tOy91slizg';
+const key = process.env.REACT_APP_NYT_API_KEY;
 
 const NytApp = () =>{
     const[ search, setSearch ] = useState('');
@@ -15,6 +15,7 @@ const NytApp = () =>{
         let url = `${baseURL}?api-key=${key}&page=${pageNumber}&q=${search}`;
         url = startDate ? url + `&begin_date=${startDate}` : url;
         url = endDate ? url + `&end_date=${endDate}` : url;
+        console.log(url);
 
         fetch(url)
         .then(res => res.json())
